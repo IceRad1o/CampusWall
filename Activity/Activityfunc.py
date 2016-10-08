c9b9175e7dd00a501a4c83a2b068a90e0c9bdced
wjl <ra7more@outlook.com>
2016-10-08 23:15:44 +0800

    活动图片url

--- Activity/Activityfunc.py
+++ Activity/Activityfunc.py
@@ -1,9 +1,28 @@
 #-*- coding:utf-8 -*-
 from Database.models import get_db
-from Database.tables import User, Favorite
+from Database.tables import User, Favorite, UserImage, Image, Activityimage
+from Image.Upload import AuthkeyHandler
 
 
 def Activityfunc(item,userphone,retdata):
+    #活动返回模型
+
+    auth = AuthkeyHandler()
+    test = get_db().query(UserImage).filter(UserImage.Uimagetel == userphone).all()
+    url=[]
+    for user_headimage in test:
+        exist = get_db().query(Image).filter(Image.IMid == user_headimage.UIimid, Image.IMvalid == 1).all()
+        if exist:
+                url = user_headimage
+                break;
+    #返回发起活动的用户的头像
+
+    aclurl = get_db().query(Activityimage).filter(Activityimage.Aimageid == item.ACid).limit(1).all()
+    #:返回活动图片
+
+
+
+
     nickname = get_db().query(User).filter(item.Acsponsorid==User.Uid).all()
     acisliked = 0
     try:
@@ -18,8 +37,9 @@ def Activityfunc(item,userphone,retdata):
     m_Activity=dict(
         Acid=item.Acid,
         Acsponsorname = nickname[0].Ualais,
-        #Acresponsorimg=item.Acsponsorimg,
         Acsponsorid=item.Acsponsorid,
+        Acsponsorimg = auth.download_url(url.Uimgurl),
+        Acimgurl = auth.download_url(aclurl[0]. Acimgurl),
         AcsponsT=item.AcsponsT.strftime('%Y-%m-%dT%H:%M:%S'),
         AccommentN=item.AccommentN,
         AclikeN=item.AclikeN,
